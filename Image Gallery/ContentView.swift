66e4f8da98d3d6e98580fc7dd8fe68304c2c6c1b
onaikepo <82336362+onaikepo@users.noreply.github.com>
2021-05-04 19:32:30 +0100

    Ui Changes no added a text field for user to add their comment.

--- Image Gallery/ContentView.swift
+++ Image Gallery/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     private var numberofImages = 7
     private let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
     @State private var currentIndex = 0
+    @State private var comment:String = " "
     
     func previous() {
         withAnimation {
@@ -64,12 +65,19 @@ struct ContentView: View {
                 controls
                     .padding()
                 Text("Product Description").font(.largeTitle).bold()
-                ScrollView{    Text("""
+                ScrollView{
+                    Text("""
                     Incredibly light and boasting a speedy performance, get your work done anywhere with the MacBook Air (2020). From video-editing to gaming, the Apple M1 chip lets you take on the biggest tasks without draining your battery.
                     It's 3.5x faster than the previous generation, with eight-cores of power providing an incredible performance. And for whisper-quiet operation, the improved thermal efficiency means it doesn't even need a fan.
                     With the Retina Display screen, everything from blockbuster movies to everyday browsing is a visual joy.
                     True Tone technology automatically adjust the display to your environment - so web pages and emails look as natural as if they were printed.
                     """) .padding() //.frame(width: 300, height: 300, alignment: .topLeading)
+                //Comment Text Field will need to go here. So it can be included within the scroll view.
+                    
+                // Rating Section will need to go her as well. SystemName:Star.circle.fill or unfill.
+                    TextField("Enter your comment here", text: $comment)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Text("You said: \(comment)")
                 }
             }
             }.navigationBarTitle("Image Gallery")
